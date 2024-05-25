d4250b5fe9b2a2b1db6476ee5dc5899f34a1b679
toygun06 <toyguny@hotmail.com>
2024-05-25 20:41:05 +0300

    add product service

--- src/app/features/products/components/product-card-list/product-card-list.component.ts
+++ src/app/features/products/components/product-card-list/product-card-list.component.ts
@@ -1,83 +1,54 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { ProductListItem } from '../../models/product-list-item';
 import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { ProductListItem } from '../../models/product-list-item';
 import { CardComponent } from '../../../../shared/components/card/card.component';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-product-card-list',
   standalone: true,
   imports: [CommonModule, CardComponent],
   templateUrl: './product-card-list.component.html',
-  styleUrl: './product-card-list.component.css'
+  styleUrl: './product-card-list.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductCardListComponent {
-
+export class ProductCardListComponent implements OnInit {
   @Input() filterByCategoryId: number | null = null;
-  @Output()viewProduct = new EventEmitter<ProductListItem>();
+  @Output() viewProduct = new EventEmitter<ProductListItem>();
 
+  productList!: ProductListItem[];
 
-  productList : ProductListItem[] =[
-    {
-      id: 1,
-      categoryId: 1, //Baverages
-      name: 'Orange Juice',
-      price: 2.99,
-      description: 'Fresh squeezed orange juice',
-      imageUrl: 'https://via.placeholder.com/200',
-    },
-    {
-      id: 2,
-      categoryId: 1, //Baverages
-      name: 'Apple Juice',
-      price: 2.99,
-      description: 'Fresh squeezed apple juice',
-      imageUrl: 'https://via.placeholder.com/200',
-    },
-    {
-      id: 3,
-      categoryId: 2, //Canned Goods
-      name: 'Tomato Soup',
-      price: 1.99,
-      description: 'Classic tomato soup',
-      imageUrl: 'https://via.placeholder.com/200',
-    },
-    {
-      id: 4,
-      categoryId: 3, //Dairy
-      name: 'Milk',
-      price: 5,
-      description: 'Whole milk',
-      imageUrl: 'https://via.placeholder.com/200',
-    },
-    {
-      id: 5,
-      categoryId: 4, // Frozen Foods
-      name: 'Frozen Pizza',
-      price: 10,
-      description: 'Pepperoni pizza',
-      imageUrl: 'https://via.placeholder.com/200',
-    },
-    {
-      id: 6,
-      categoryId: 5, //Meat
-      name: 'Ground Beef',
-      price: 8,
-      description: 'Ground beef',
-      imageUrl: 'https://via.placeholder.com/200',
-    },
-  ] //Mock Data
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.getProductList();
+  }
+
+  getProductList() {
+    const request = this.productsService.getList().subscribe((productList) => {
+      this.productList = productList;
+      request.unsubscribe();
+    });
+  }
 
   onViewProduct(product: ProductListItem) {
     this.viewProduct.emit(product);
-    }
+  }
 
   get filteredProductList(): ProductListItem[] {
     let filteredProductList = this.productList;
 
-    if(this.filterByCategoryId) {
+    if (this.filterByCategoryId) {
       filteredProductList = this.productList.filter(
         (product) => product.categoryId === this.filterByCategoryId
-      )
+      );
     }
 
     return filteredProductList;
---
+++ src/app/features/products/services/products.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductListItem } from '../models/product-list-item';
+import { ProductDetail } from '../models/product-detail';
+
+@Injectable({
+  providedIn: 'root',
+}) // singleton
+export class ProductsService {
+  private apiControllerUrl = 'http://localhost:3000/products';
+
+  constructor(private http: HttpClient) {}
+
+  getList(): Observable<ProductListItem[]> {
+    return this.http.get<ProductListItem[]>(this.apiControllerUrl);
+  }
+
+  getDetail(id: number) : Observable<ProductDetail> {
+    return this.http.get<ProductDetail>(`${this.apiControllerUrl}/${id}`);
+  }
+}
