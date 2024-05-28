d41236d5dd01c0712f91e006e1a39607133545d8
toygun06 <toyguny@hotmail.com>
2024-05-29 02:24:17 +0300

    add new category form component and related files

--- src/app/features/categories/services/categories.service.ts
+++ src/app/features/categories/services/categories.service.ts
@@ -1,8 +1,10 @@
+import { CategoryListItem } from './../models/category-list.item';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { CategoryListItem } from '../models/category-list.item';
+import { NewCategory } from '../models/new-category';
+import { AddedCategory } from '../models/added-category';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +15,10 @@ export class CategoriesService {
 
   getList() : Observable<CategoryListItem[]> {
     return this.http.get<CategoryListItem[]>(this.apiControllerUrl)
+  }
+
+  add(category: NewCategory): Observable<AddedCategory> {
+    return this.http.post<AddedCategory>(this.apiControllerUrl,
+      category);
   }
 }
