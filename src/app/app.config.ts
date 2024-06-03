513845e9845ce2d6c4ceb6bce0e95d721b07ae91
Kemal Toygun Yıldız <toyguny@hotmail.com>
2024-06-03 16:14:17 +0300

    fixed loading.service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/auth/interceptors/auth.interceptor';
-import { LoadingInterceptor } from './features/loading/loading.interceptop';
+import { LoadingInterceptor } from './features/loading/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration(),provideHttpClient(withInterceptors([authInterceptor])),{ provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true }]
